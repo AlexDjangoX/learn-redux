99c2d54238b030df1486e310287c922168d7a10c
AlexDjangoX <alexmonk17@gmail.com>
2022-09-24 16:14:29 +0100

    EditPostForm component added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import PostsList from './features/posts/PostsList';
 import AddPostForm from './features/posts/AddPostForm';
 import SinglePostPage from './features/posts/SinglePostPage';
 import Layout from './components/Layout';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<PostsList />} />
 
-      <Route index element={<PostsList />} />
+        <Route path='post'>
+          <Route index element={<AddPostForm />} />
+          <Route path=':postId' element={<SinglePostPage />} />
+        </Route>
 
-      <Route path='post'>
-        <Route index element={<AddPostForm />} />
-        <Route path=':postId' element={<SinglePostPage />} />
-        <Route />
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Route>
-
-      <Route />
     </Routes>
   );
 }
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -4,9 +4,12 @@ import { selectPostById } from './postsSlice';
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
 import ReactionButtons from './ReactionButtons';
+import { useParams, Link } from 'react-router-dom';
 
 const SinglePostPage = () => {
-  const post = useSelector((state) => selectPostById(state, postId));
+  const { postId } = useParams();
+
+  const post = useSelector((state) => selectPostById(state, Number(postId)));
 
   if (!post) {
     return (
@@ -21,6 +24,7 @@ const SinglePostPage = () => {
       <h3>{post.title}</h3>
       <p>{post.body}</p>
       <p className='postCredit'>
+        <Link to={`post/edit/${post.id}`}>Edit Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
